e7c830b0f7c148ae527d842ada1c240b52d92721
Bogatyreva Anastasia <bogatyreva@bpcbt.com>
2020-07-24 08:14:16 +0300

    Added hw3

---
+++ hw1/src/hw3/GuessNumberGame.java
@@ -0,0 +1,41 @@
+package hw3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumberGame {
+    static Scanner sc = new Scanner(System.in);
+    static int lifes = 3;
+
+    public static void main(String[] args) {
+        do {
+            playGame();
+        }
+        while (sc.nextInt() != 0);
+    }
+
+    public static int getRandomNumber(int bound, int shift) {
+        Random random = new Random();
+        return random.nextInt(bound) - shift;
+    }
+
+    public static void playGame() {
+        int num = getRandomNumber(10, 0);
+        lifes = 3;
+
+        System.out.print("\nПопробуйте угадать число от 0 до 9. У вас " + lifes + " попытки: ");
+        while (lifes > 0) {
+            int ans = sc.nextInt();
+
+            if (ans == num) {
+                System.out.println("Вы угадали! Повторить игру еще раз? (1 – да / 0 – нет)");
+                return;
+            }
+
+            lifes--;
+            System.out.print("У вас осталось попыток: " + lifes + ". Попробуйте еще раз: ");
+        }
+        System.out.println("У вас больше нет попыток. Повторить игру еще раз? (1 – да / 0 – нет)");
+    }
+
+}
---
+++ hw1/src/hw3/GuessWordGame.java
@@ -0,0 +1,55 @@
+package hw3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * При запуске программы компьютер загадывает слово, запрашивает ответ у пользователя,
+ * сравнивает его с загаданным словом и сообщает правильно ли ответил пользователь.
+ * Если слово не угадано, компьютер показывает буквы которые стоят на своих местах.
+ * apple – загаданное, apricot - ответ игрока
+ * ---> ap############# (15 символов, чтобы пользователь не мог узнать длину слова)
+ */
+public class GuessWordGame {
+    static String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+
+    public static void main(String[] args) {
+        String word = getRandomWord(words.length, 0);
+        String hiddenWord = "###############";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Попробуйте угадать слово: ");
+        while (true) {
+            String ans = scanner.nextLine();
+
+            if (ans.equals(word)) {
+                break;
+            }
+
+            hiddenWord = openRightSymbols(word, ans, hiddenWord);
+            System.out.print("\nВы угадали:" + hiddenWord + "\nПопробуйте еще раз: ");
+
+        }
+
+        System.out.println("\nВы угадали!");
+        scanner.close();
+    }
+
+    public static String getRandomWord(int bound, int shift) {
+        Random random = new Random();
+        return words[random.nextInt(bound) - shift];
+    }
+
+    public static String openRightSymbols(String right, String answer, String hidden) {
+        StringBuilder sb = new StringBuilder(hidden);
+        int length = Math.min(right.length(), answer.length());
+
+        for (int i = 0; i < length; i++) {
+            if (right.charAt(i) == answer.charAt(i)) {
+                sb.replace(i, i + 1, Character.toString(answer.charAt(i)));
+            }
+        }
+
+        return sb.toString();
+    }
+}

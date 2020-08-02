9fd0f4eee8839278820f095bb5d3500e648bd57f
Bogatyreva Anastasia <bogatyreva@bpcbt.com>
2020-08-02 23:23:57 +0300

    Added hw5

---
+++ hw1/src/hw5/Data.java
@@ -0,0 +1,10 @@
+package hw5;
+
+public class Data {
+    public static final String[] FIOS = {"Ivanov Ivan", "Sidorov Alexey", "Afonin Grigory", "Lukina Natalia", "Romanova Vasilisa"};
+    public static final String[] POSTS = {"Developer", "Product manager", "Developer", "Testing Engineer", "Designer"};
+    public static final String[] EMAILS = {"ii@mailbox.com", "sa@mailbox.com", "ag@mailbox.com", "ln@mailbox.com", "rv@mailbox.com"};
+    public static final String[] PHONES = {"89165554433", "89165554343", "89165552277", "89165551122", "89165551212"};
+    public static final double[] SALARIES = {140.6, 200.4, 120.9, 140.2, 150.9};
+    public static final int[] AGES = {35, 50, 29, 25, 44};
+}
